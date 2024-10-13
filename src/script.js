fc079f97dcfa66a5b2d5a88e133c634ab2848667
Min Naing Oo <minnaingoo792@gmail.com>
2024-10-13 08:45:14 +0630

    HOTEL-navbar_section_done

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const menubar = document.getElementById('menubar');
+console.log(menubar);
+
+const navlinks = document.getElementById('navlinks');
+console.log(navlinks);
+
+
+
+menubar.addEventListener('click', ()=>{
+    navlinks.classList.toggle('show');
+})
